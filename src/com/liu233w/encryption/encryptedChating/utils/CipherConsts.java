e9c3ab49413734cdf3c5b0f5e76bb2af0547f494
Liu233w <wwwlsmcom@outlook.com>
2018-06-13 09:42:56 +0800

    t

---
+++ src/com/liu233w/encryption/encryptedChating/utils/CipherConsts.java
@@ -0,0 +1,9 @@
+package com.liu233w.encryption.encryptedChating.utils;
+
+public class CipherConsts {
+    public static final String RsaAlgorithm = "RSA";
+
+    public static final int RsaKeySize = 1024;
+
+    public static final String DesAlgorithm = "DES";
+}
