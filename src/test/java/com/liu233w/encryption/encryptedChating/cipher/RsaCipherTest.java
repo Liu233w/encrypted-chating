055e0bfb4a5970906096d01c3efd64edf5ce8b3e
Liu233w <wwwlsmcom@outlook.com>
2018-06-20 08:48:23 +0800

    修复测试
    之前的 RsaCipherTest 错误的命名成了 DesCipherTest

---
+++ src/test/java/com/liu233w/encryption/encryptedChating/cipher/RsaCipherTest.java
@@ -0,0 +1,22 @@
+package com.liu233w.encryption.encryptedChating.cipher;
+
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class RsaCipherTest {
+
+
+    @Test
+    public void testEncryptAndDecrypt() {
+        final com.liu233w.encryption.encryptedChating.cipher.RsaKeyPair rsaKeyPair = new com.liu233w.encryption.encryptedChating.cipher.RsaKeyPair(BigInteger.valueOf(3 * 11), BigInteger.valueOf(3), BigInteger.valueOf(7));
+
+        assertThat(RsaCipher.encrypt(BigInteger.valueOf(24).toByteArray(), rsaKeyPair.getPublicKey()))
+                .isEqualTo(BigInteger.valueOf(30).toByteArray());
+
+        assertThat(RsaCipher.encrypt(BigInteger.valueOf(30).toByteArray(), rsaKeyPair.getPrivateKey()))
+                .isEqualTo(BigInteger.valueOf(24).toByteArray());
+    }
+}
