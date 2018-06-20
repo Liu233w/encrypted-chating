c370cdc804c8cb69de0df63877470e48d7d811d0
Liu233w <wwwlsmcom@outlook.com>
2018-06-20 18:39:28 +0800

    add rsa key parser

--- src/main/java/com/liu233w/encryption/encryptedChating/cipher/RsaKey.java
+++ src/main/java/com/liu233w/encryption/encryptedChating/cipher/RsaKey.java
@@ -25,4 +25,15 @@ public class RsaKey {
     public String toString() {
         return String.format("[%s,%s]", n.toString(), e.toString());
     }
+
+    /**
+     * Parse the input as RsaKey, don't have any error checking
+     *
+     * @param input what toString() outputs
+     * @return
+     */
+    public static RsaKey parse(String input) {
+        final String[] split = input.split("[\\[\\],]");
+        return new RsaKey(new BigInteger(split[1]), new BigInteger(split[2]));
+    }
 }
--- src/test/java/com/liu233w/encryption/encryptedChating/cipher/RsaCipherTest.java
+++ src/test/java/com/liu233w/encryption/encryptedChating/cipher/RsaCipherTest.java
@@ -11,12 +11,18 @@ public class RsaCipherTest {
 
     @Test
     public void testEncryptAndDecrypt() {
-        final com.liu233w.encryption.encryptedChating.cipher.RsaKeyPair rsaKeyPair = new com.liu233w.encryption.encryptedChating.cipher.RsaKeyPair(BigInteger.valueOf(3 * 11), BigInteger.valueOf(3), BigInteger.valueOf(7));
+        final RsaKeyPair rsaKeyPair = new RsaKeyPair(BigInteger.valueOf(3 * 11), BigInteger.valueOf(3), BigInteger.valueOf(7));
 
         assertThat(RsaCipher.encrypt(BigInteger.valueOf(24).toByteArray(), rsaKeyPair.getPublicKey()))
                 .isEqualTo(BigInteger.valueOf(30).toByteArray());
 
         assertThat(RsaCipher.encrypt(BigInteger.valueOf(30).toByteArray(), rsaKeyPair.getPrivateKey()))
                 .isEqualTo(BigInteger.valueOf(24).toByteArray());
+    }
+
+    @Test
+    public void testRsaKeyParse() {
+        final RsaKey key = RsaKey.parse("[123456,78900]");
+        assertThat(key.toString()).isEqualTo("[123456,78900]");
     }
 }
